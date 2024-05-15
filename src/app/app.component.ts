953b3965101226cb75136c2124c587c13f3bbcf8
AlexandrosChrissafacisPXL <alexandros.chrissafacis@bewire.be>
2024-05-15 10:39:25 +0200

    added todoitem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import {Todo} from "./models/todo-model";
 })
 export class AppComponent {
   public todos : Todo[] = [
-    {id: 1, title: 'Install Angular CLI', completed: false},
+    {id: 1, title: 'Install Angular CLI', completed: true},
     {id: 2, title: 'Style app', completed: false},
     {id: 3, title: 'Finish service functionality', completed: false},
     {id: 4, title: 'Setup API', completed: false},
